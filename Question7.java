305115c0e3e4ab49c8fd3ae63b48d01d45f1acd7
NIRVIKAR <nirvikarprasoon@gmail.com>
2023-09-21 00:24:10 +0530

    Solved

---
+++ Question7.java
@@ -0,0 +1,46 @@
+//Write a program for overloading operator++ and operator—using friend functions
+class OpOverload {
+    private int value;
+
+    OpOverload(int val) {
+        this.value = val;
+    }
+
+    public void increment() {
+        this.value++;
+    }
+
+    public void decrement() {
+        this.value--;
+    }
+
+    public void getValue() {
+        System.out.println(this.value);
+    }
+}
+
+public class Question7 {
+    public static void main(String[] args) {
+        OpOverload op1 = new OpOverload(20);
+        System.out.println("Original Value");
+        op1.getValue();
+        op1.increment();
+        System.out.println("Value after increment");
+        op1.getValue();
+        op1.decrement();
+        System.out.println("Value after decrement");
+        op1.getValue();
+    }
+
+}
+
+/*
+ * 
+ * 
+ * Original Value
+ * 20
+ * Value after increment
+ * 21
+ * Value after decrement
+ * 20
+ */
