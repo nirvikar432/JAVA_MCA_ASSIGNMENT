cd91703784c5c8003e82610e42f5be6e7ddd2e50
NIRVIKAR <nirvikarprasoon@gmail.com>
2023-10-14 23:14:20 +0530

    Initial Commit

---
+++ Question13.java
@@ -0,0 +1,7 @@
+//Write a program to read the class object of student info such as name , age ,sex ,height 
+// and weight from the keyboard and to store them on a specified file using read() and write() 
+// functions. Again the same file is opened for reading and displaying the contents of the file on the screen.
+
+public class Question13 {
+
+}
